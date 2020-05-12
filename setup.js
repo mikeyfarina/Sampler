7cc2dc483fdab2cba4a8e0d3a1715bd50577ae8d
mikeyfarina <mikeyfarina7@gmail.com>
2020-05-12 18:54:37 -0400

    remove auto scroll

--- setup.js
+++ setup.js
@@ -6,7 +6,4 @@ export function init(){
   loadSamples();
   setupUploadButtons();
   screenSubtitle.innerHTML = "ready to play";
-  window.addEventListener("load", function() {
-    window.scrollTo(1, 0);
-  }, false);
 }
