d908804ebed276fcac7e0a0c82af23e9c122b351
mikeyfarina <mikeyfarina7@gmail.com>
2020-05-12 18:50:54 -0400

    fix size change when signfying samples loaded

--- setup.js
+++ setup.js
@@ -5,7 +5,7 @@ import {screenSubtitle} from "./constants.js";
 export function init(){
   loadSamples();
   setupUploadButtons();
-  screenSubtitle.innerHTML = "ready to play!";
+  screenSubtitle.innerHTML = "ready to play";
   window.addEventListener("load", function() {
     window.scrollTo(1, 0);
   }, false);
