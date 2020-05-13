b754afe9dcba45cb523e7ea81300fb0715a54751
mikeyfarina <mikeyfarina7@gmail.com>
2020-05-13 13:42:14 -0400

    add auto scroll

--- setup.js
+++ setup.js
@@ -6,4 +6,12 @@ export function init(){
   loadSamples();
   setupUploadButtons();
   screenSubtitle.innerHTML = "ready to play";
+
+  window.addEventListener("load",function() {
+    // Set a timeout...
+    setTimeout(function(){
+      // Hide the address bar!
+      window.scrollTo(0, 1);
+    }, 0);
+  });
 }
