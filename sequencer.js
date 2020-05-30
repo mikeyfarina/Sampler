5f09a2fee6a842dd65eb2dbc85103f36240aafc6
mikeyfarina <mikeyfarina7@gmail.com>
2020-05-30 19:05:02 -0400

    works, has some stuttering when stopping and starting, there is prob a better solution

--- sequencer.js
+++ sequencer.js
@@ -1,5 +1,6 @@
 import {context, beatSelector, tempoSlider, tempoDisplay} from "./constants.js";
 import {makeSource, loadedPadsWithSamples} from "./setupPads.js";
+import {trackObject} from "./setupSeqTracks.js";
 
 let isPlaying = false;    // Are we currently playing?
 let current16thNote;    // What note is currently last scheduled?
@@ -20,16 +21,16 @@ let notesInQueue = [];      // the notes that have been put into the web audio,
 let seqTracks;
 
 export function setUpSequencer(){
-  console.log("\n\nSEQUENCER LOGS\n\n\n")
   let playButton = document.querySelector('.sequencer__controls__play-button');
 
   console.log("playButton", playButton);
   playButton.addEventListener("click", (ev)=>{
+    console.log("\n\nSEQUENCER LOGS\n\n\n");
     console.log("clicked playbutton");
-    [].forEach.call(seqTracks, (track) =>{
-      console.log("setup loop calling play", track);
-      play(ev, track);
-    })
+    //[].forEach.call(seqTracks, (track) =>{
+      //console.log("setup loop calling play", track);
+      play(ev);
+    //})
   });
   
   // First, let's shim the requestAnimationFrame API, with a setTimeout fallback
@@ -48,6 +49,8 @@ export function setUpSequencer(){
 }
 
 function nextNote() {
+  console.log("next note");
+
   tempo = tempoSlider.value;            //always updating the tempo
   tempoDisplay.innerText = tempo;       //and note resolution 
   noteResolution = beatSelector.selectedIndex;
@@ -62,33 +65,40 @@ function nextNote() {
   }
 }
 
-function scheduleNote( beatNumber, time, buffer, trackButtons) {
+function scheduleNote( beatNumber, time) {
+  console.log("scheduleNote log");
   //get note res
   noteResolution = beatSelector.selectedIndex;
 
   // push the note on the queue, even if we're not playing.
   notesInQueue.push( { note: beatNumber, time } );
-  console.log("scheduling note", {beatNumber, time} , notesInQueue);
+  console.log("scheduling note ", beatNumber , notesInQueue);
 
   if ( (noteResolution==1) && (beatNumber%2))
     return; // we're not playing non-8th 16th notes
   if ( (noteResolution==2) && (beatNumber%4))
     return; // we're not playing non-quarter 8th notes
 
-  console.log("scheduleNote log");
-  console.log("trackButtons",trackButtons);
-  console.log("beatNumber " + beatNumber);
-  console.log("trackButtons " + trackButtons); 
-  console.log("trackButtons[beatNumber] " + trackButtons[beatNumber]);
-  console.log("contains(\"clicked\").classList.contains(\"clicked\"): ");
+  [].forEach.call(seqTracks, (track)=>{
+    let trackButtons = track.querySelectorAll(".sequencer__display__track__button");
+    let name = track.querySelector("span").innerText;
 
-  console.log("schedulenote end", beatNumber, trackButtons, buffer)
-  if (trackButtons[beatNumber] === undefined){
-    console.log("beat undefined");
-  }else if (trackButtons[beatNumber].classList.contains("clicked")){
-    console.log("playing buffer");
-    playSample(buffer);
-  }
+    console.log(
+      "contains(\"clicked\").classList.contains(\"clicked\"): ",
+      trackButtons[beatNumber].classList.contains("clicked"),
+      trackButtons[beatNumber],
+      name
+    );
+
+    let trackInfo = trackObject.find(o => o.trackName === name);
+    console.log("loop trackInfo",trackInfo);
+
+    if (trackButtons[beatNumber].classList.contains("clicked")){
+      console.log("!! playing buffer", trackInfo.trackBuffer);
+      let buffer = trackInfo.trackBuffer;
+      playSample(buffer);
+    }
+  });
 
   //if ( !(beatNumber % 16) || !(beatNumber % 8) ) // beat 0 == low pitch
     //playSample(listOfSamples[2]);
@@ -104,30 +114,25 @@ function playSample(buffer){
   source.source.start(0);
 }
 
-function scheduler(trackBuffer, trackButtons) {
+function scheduler() {
 
   while (nextNoteTime < context.currentTime + scheduleAheadTime ) {
-    scheduleNote( current16thNote, nextNoteTime,trackBuffer, trackButtons);
+    scheduleNote( current16thNote, nextNoteTime);
     nextNote();
   }
   
   timerID = window.setTimeout( scheduler, lookahead );
 }
 
-function play(ev, track) {
-  console.log("play called");
-  let trackButtons = track.querySelectorAll(".sequencer__display__track__button");
-
+function play(ev) {
+  console.log("play", trackObject, isPlaying);
   isPlaying = !isPlaying;
 
   if (isPlaying) { // start playing
     current16thNote = 0;
     nextNoteTime = context.currentTime;
-    
-    console.log("loop track", track, "trackButtons",trackButtons);
-    // kick off scheduling
-    scheduler(track, getBufferOfTrack(track), trackButtons);  
-    
+    //kick off scheduling
+    scheduler();  
     ev.target.innerText = "stop";
   } else {
     window.clearTimeout( timerID );
@@ -150,26 +155,32 @@ function draw() {
   // We only need to draw if the note has moved.
   if (last16thNoteDrawn != currentNote) {
     last16thNoteDrawn = currentNote;
+    for(let track of seqTracks){
       for (let i=0; i<16; i++) {
-        seqTracks[0].children[i+1].style.background = ( currentNote == i ) ?
-          (( currentNote % 4 == 0 ) ? "#4880ff" : "white" ) : "#7c7c7c"; 
+        //seqTracks[0].children[i+1].style.background = ( currentNote == i ) ?
+          //(( currentNote % 4 == 0 ) ? "#4880ff" : "white" ) : "#7c7c7c"; 
         if (currentNote == i){
           (currentNote % 4 == 0) ?
-            seqTracks[0].children[i+1].style.background = "#4880ff" :
-           seqTracks[0].children[i+1].style.background = "white";
+            track.children[i+1].style.background = "#4880ff" 
+          : track.children[i+1].style.background = "white";
+
         } else {
-          seqTracks[0].children[i+1].style.background = "#7c7c7c";
+          track.children[i+1].style.background = "#7c7c7c";
         }
       }
+    }
   }
   // set up to draw again
   requestAnimFrame(draw);
 }
 
 function getBufferOfTrack(track){
+  console.log("getBuffer of track");
   let trackName = track.querySelector(".sequencer__display__track__name").innerText;
+  console.log("track name", trackName)
   for (let pad of loadedPadsWithSamples){
-    if (pad.buffer.name === trackName){
+    if (pad.buffer.name === trackName) {
+      console.log("returning..", pad.buffer)
       return pad.buffer;
     }
   }
